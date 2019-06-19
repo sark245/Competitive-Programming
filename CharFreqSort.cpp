0b7f4654049ba15bec201e3a14cb2a14da36be5a
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2019-06-19 22:00:10 +0530

    To sort a string in descending order on the basis of frequency of it's characters

---
+++ CharFreqSort.cpp
@@ -0,0 +1,20 @@
+//
+// Created by sark2 on 19-06-2019.
+//
+
+#include<iostream>
+using namespace std;
+
+// -----------------------------------------------------
+// This is a functional problem. Only this function has to be written.
+string frequencySort(string s) {
+    //Write your code here
+
+}
+
+//Dont make changes here
+int main(int argc, char** argv){
+    string s;
+    cin>>s;
+    cout<<frequencySort(s);
+}
