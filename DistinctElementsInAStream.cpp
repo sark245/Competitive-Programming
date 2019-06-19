0002d72f35381642f522dfca2d71ffbb960359d4
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2019-06-19 21:48:53 +0530

    Count Distinct Elements in a stream after the given operations

---
+++ DistinctElementsInAStream.cpp
@@ -0,0 +1,55 @@
+//
+// Created by sark2 on 19-06-2019.
+//
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class  operation{
+public:
+    char op;
+    int key;
+
+    operation(char op,int key){
+        this->key=key;
+        this->op=op;
+    }
+
+};
+
+//Don't make any changes here
+
+void display(vector<operation>& list) {
+
+    for (int i = 0; i < list.size(); i++) {
+        cout<<list[i].op<<" "<<list[i].key<<endl;
+    }
+    cout<<endl;
+}
+
+//this is a functional problem
+
+void print(vector<operation> & v){
+    //  Write your code here
+
+}
+
+
+int main(int argc,char** argv){
+    // Input for length of array.
+    int N ;
+    cin>>N;
+    vector<operation> v;
+
+    // Input for array elements.
+    for (int i = 0; i < N; i++) {
+        char a;
+        int k;
+        cin>>a>>k;
+        operation ob(a,k);
+        v.push_back(ob);
+    }
+
+    print(v);
+}
