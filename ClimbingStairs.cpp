b9192997bef1f933a905f268964fd79967ba61ed
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2019-07-06 00:42:55 +0530

    Climbing Stairs Dp

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,17 @@
+//
+// Created by sark2 on 05-07-2019.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+#define IOS cin.sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define cases int t;cin>>t;while(t--)
+typedef long double ld;
+
+int main() {
+    IOS
+
+
+
+}
