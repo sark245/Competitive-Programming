0c6e640453fe7274937cf9aa85d491b6378e82c2
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2019-07-07 22:25:04 +0530

    Coin Change Problem

---
+++ CoinChange.cpp
@@ -0,0 +1,46 @@
+//
+// Created by sark2 on 06-07-2019.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+#define IOS cin.sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define cases int t;cin>>t;while(t--)
+typedef long double ld;
+
+int main() {
+    IOS
+
+    cases{
+     int m;
+     cin>>m;
+     int a[m+1];
+     for(int i = 0; i < m; i++){
+         cin>>a[i];
+     }
+     int N;
+     cin>>N;
+
+     int dp[N+1][m];
+     memset(dp,0,sizeof dp);
+
+     for(int i = 0; i < m; i++){
+         dp[0][i] = 1;
+     }
+
+     for(int i = 1; i < N+1; i++){
+         for(int j = 0; j < m; j++){
+
+             int x =  (i-a[j] >= 0)?dp[i-a[j]][j] : 0;
+             int y = (j>=1)?dp[i][j-1] : 0;
+             dp[i][j] = x+y;
+
+         }
+     }
+
+     cout<<dp[N][m-1];
+
+    }
+
+}
