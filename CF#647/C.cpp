a089d9fa83de43daf511f3e8c0b6a024991b7998
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-06-06 20:42:00 +0530

    Create C.cpp

---
+++ CF#647/C.cpp
@@ -0,0 +1,52 @@
+//Created by Kira
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS cin.sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define pii pair<int,int> 
+#define pb push_back
+#define vi vector<int> 
+using ll = long long;
+
+const int mod = 1e9 + 7;
+
+ll pwr(ll a, ll b);
+    
+
+
+
+int main(){
+   IOS
+
+    int t; cin>>t;
+    while(t--){
+        ll n; cin>>n;
+
+        ll c = 1, ans = 0;
+       while(n){
+           ans += c * ((n + 1)/2);
+           n /= 2;
+           c++;
+       }
+
+       cout<<ans<<"\n";
+    }
+
+}
+
+ll pwr(ll a, ll b){
+   a %= mod;	//Remove mod if not required
+   ll res = 1;
+   while(b > 0){
+    if(b&1)
+        res = res * a % mod;
+        a = a * a % mod;
+    b >>= 1;
+    }
+
+    return res;
+}
+
