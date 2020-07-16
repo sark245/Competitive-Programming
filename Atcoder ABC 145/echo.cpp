becfe8a5fb350e8281cda2b833dd0d8f00cb2894
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-07-17 02:31:36 +0530

    Create echo.cpp

---
+++ Atcoder ABC 145/echo.cpp
@@ -0,0 +1,53 @@
+//Created by Kira
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS cin.sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define pii pair<int,int> 
+#define pb push_back
+#define vi vector<int> 
+using ll = long long;
+
+const int mod = 1e9 + 7;
+
+ll pwr(ll a, ll b);
+    
+
+
+
+int main(){
+   IOS
+
+    int n; cin>>n;
+    string s; cin>>s;
+
+    if(n & 1){
+        cout<<"No\n";
+    }else{
+
+        if(s.substr(0, n/2) == s.substr(n/2, n/2)){
+            cout<<"Yes\n";
+        }
+        else{
+            cout<<"No\n";
+        }
+
+    }
+
+}
+
+ll pwr(ll a, ll b){
+   a %= mod;	//Remove mod if not required
+   ll res = 1;
+   while(b > 0){
+    if(b&1) res = res * a % mod;
+        a = a * a % mod;
+    b >>= 1;
+    }
+
+    return res;
+}
+
