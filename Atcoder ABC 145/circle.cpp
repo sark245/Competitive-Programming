8b6d6b74c36cdd9deb5b9945561fc549e487e2ee
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-07-17 02:31:13 +0530

    Create circle.cpp

---
+++ Atcoder ABC 145/circle.cpp
@@ -0,0 +1,41 @@
+//Created by Kira
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS cin.sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define pii pair<int,int> 
+#define pb push_back
+#define vi vector<int> 
+using ll = long long;
+
+const int mod = 1e9 + 7;
+
+ll pwr(ll a, ll b);
+    
+
+
+
+int main(){
+   IOS
+
+    int r; cin>>r;
+    cout<<r * r<<"\n";
+    
+
+}
+
+ll pwr(ll a, ll b){
+   a %= mod;	//Remove mod if not required
+   ll res = 1;
+   while(b > 0){
+    if(b&1) res = res * a % mod;
+        a = a * a % mod;
+    b >>= 1;
+    }
+
+    return res;
+}
+
